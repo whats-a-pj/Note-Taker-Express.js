41b726b3431184b28f3ecb9f0f21ee0dc7492de3
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-09-04 13:24:07 -0600

    Cannot GET /notes, trying to parse data or SOMETHING idk

--- server.js
+++ server.js
@@ -1,39 +1,45 @@
-// require fs modules and express()
-//writeToFile index.html? with template literals n such
-
-//todo refresh data persistence activities in UofU repo
+//this helps connect express and create paths
 const express = require("express");
 const path = require("path");
-const noteDB = require('./db/db.json');
+const noteDB = require('./develop/db/db.json');
 const fs = require("fs");
 
-const PORT = 3001;
-
 const app = express();
+
+const PORT = process.env.PORT || 3001;
+// const PORT = "127.0.0.1";
 
 //this is the middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-app.use(express.static('./public'));
+app.use(express.static('public'));
 // app.use('/public');
 
 //todo where to input the fs.readFile??
+//todo where to put app.post and how do i parse my db.json data?
+
+//connecting the notes db
+// app.get('/api/notes', (req, res) => {
+//     res.json(noteDB);
+// });
 
 //connecting the index.html
-app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 //connecting the notes.html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//connecting the index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 //connecting the localhost port
-app.listen(PORT, console.log(`Port working properly at ${PORT} ???`));
-// app.listen(PORT, (req, res) => {
-//     res.send(`port is working properly at ${PORT} ???`);
-// })
+app.listen(PORT, console.log(`Port working properly at ${PORT}`));
 
 
 /********************************************************************************************/
