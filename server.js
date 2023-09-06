2b397dca6fb694177f7404611d55f1501493495c
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-09-05 22:43:29 -0600

    Working properly now, might add delete feature now

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //this helps connect express and create paths
 const express = require("express");
 const path = require("path");
-const uuid = require("uuid");
-//const noteDB = require('./db/db.json'); ?? askbcs says i dont need this but not sure why not
+const { v4: uuidv4 } = require('uuid');
+const noteDB = require('./db/db.json');
 const fs = require("fs");
 
 const app = express();
@@ -16,45 +16,52 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile("./db/db.json", uuid, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.status(404).json({ error: "something ain't right here buddy"});
-        } else {
-            res.json(data);
-            // res.json(JSON.parse(data));
-        }
-    })
-   // res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
-app.post('/api/notes', (req, res) => {
-    fs.readFile("./db/db.json", uuid, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.status(500).json({err: "something ain't right here buddy"});
-        } else {
-            res.json(JSON.parse(data));
-        }
-    });
+app.post("/api/notes", (req, res) => {
 
-    //todo fs.writeFile here & parse it
-    //1. pull current note data
-    //2. add new note data
-    //3. req.body.(title/text/id from db.json) how do? .push somewhere 
-    //4. fs.writeFile db.json -- res.json w uuid()
-/****************************************************************
-    fs.writeFile("./db/db.json", uuid, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.status(500).json({err: "something's broken yo"});
-        } else {
-            res.json(JSON.parse(data));
-        }
-        })
-********************************************************************/
-
+    const newNote = req.body;
+    console.log("New Note : " + JSON.stringify(newNote));
+    newNote.id = uuidv4();
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    data.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(data));
+    console.log("Added new note to 'db.json' file!");
+    res.json(data);
 });
+
+// app.get('/api/notes', (req, res) => {
+//     fs.readFileSync("./db/db.json", (err, data) => {
+//         if (err) {
+//             console.log(err);
+//             res.status(500).json({ error: "something ain't right here buddy"});
+//         } else {
+//             res.json(JSON.stringify(data));
+//             // res.json(JSON.parse(data));
+//         }
+//     })
+//    // res.sendFile(path.join(__dirname, './public/notes.html'));
+// });
+
+//todo fs.writeFile POST request here & parse it somehow
+    //1. pull current note data 
+    //- readFileSync- noteDB     //readFileSync async func/async await- loads up before anything else is done
+    //2. add new note data let newNote is another key in db.json
+    //- use noteDB to add newNotes and parse it with a .push and stringify - req.body.(title/text/id from db.json) how do? .push somewhere 
+    //- need to create id
+    //3. fs.writeFile db.json -- res.json w uuid()
+    //- JSON.parse(fs.readFileSync(./db/db.json or noteDB)
+    //- api/notes/:id
+//     let newNotes = JSON.stringify(fs.writeFileSync("./db/db.json", (err, noteDB) => {
+//         if (err) {
+//             console.log(err);
+//             res.status(500).json({err: "something's broken yo"});
+//         } else {
+//             res.json(JSON.parse(noteDB.push(newNotes)));
+//         }
+//         }));
+/************************************************************************************************************************************/
 
 //connecting the index.html
 app.get('/', (req, res) => {
@@ -76,6 +83,30 @@ app.listen(PORT, console.log(`Port working properly at ${PORT}`));
 
 
 /********************************************************************************************/
+//todo this is literally all just parsing notes and garbage that isn't working
+// app.post("/api/notes", (req, res) => {
+
+//     const newNote = req.body;
+//     console.log("New Note : " + JSON.stringify(newNote));
+//     newNote.id = uuidv4();
+//     let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+//     data.push(newNote);
+//     fs.writeFileSync('./db/db.json', JSON.stringify(data));
+//     console.log("Added new note to 'db.json' file!");
+//     res.json(data);
+// });
+// app.post('/api/notes', (req, res) => {
+    //readFileSync here
+//     const notes = JSON.stringify(fs.readFileSync("./db/db.json", (err, data) => {
+//         if (err) {
+//             console.log(err);
+//             res.status(500).json({err: "something ain't right here buddy"});
+//         } else {
+//             res.json(JSON.parse(data.JSON.stringify(notes)));
+//         }
+//     }));
+
+// });
 
         // if (err) {
         //     console.log(err);
