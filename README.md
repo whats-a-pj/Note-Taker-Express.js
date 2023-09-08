bc2ad37fbd21217f7c809d6ce36979909b41a6ea
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-09-07 22:39:25 -0600

    Updated readme & deployed application

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # Note-Taker-Express.js
-An application that can be used to write and save notes. This uses an Express.js back end that will save and retrieve note data from a JSON file.
+An application that can be used to write and save notes. This uses an Express.js back end that will save and retrieve note data from a JSON file. You can use this for grocery lists, to-do lists, quick notes to look back at later or general reminders.
+
+## Table of Contents
+* [Installation](#installation)
+* [Additional Information](#additional-information)
+
+## Installation
+- This needs to be run on your local machine
+
+- After cloning this repo down locally, make sure you have node and express downloaded
+
+- Then if you have nodemon installed you can run nodemon server.js or if you do not have nodemon, just node server.js and that should start up your server 
+
+- Open a new tab on your web browser and type in "localhost:3001" and the application will be there
+
+- Add as many notes as you like!
+
+
+## Additional Information
+
+> ### Repository Owner: P.J. Rasmussen ([whats-a-pj](https://github.com/whats-a-pj)) 
+
+<details>
+<summary> Screenshot of Site & Repo Links </summary>
+
+
+
+</details>
+
+<details>
+<summary> Process Summary </summary>
+
+Downloaded starter code
+
+Spent time researching Express and re-watching class zoom links
+
+Set up routes and files in a way that made sense to me
+
+Looked up docs on UUID for unique ids
+
+Updated README & Deployed site
+
+</details>
+
+<details>
+<summary> Credits </summary>
+
+These are the websites I used including the starter code repo
+
+https://github.com/coding-boot-camp/miniature-eureka
+
+https://expressjs.com/en/guide/routing.html
+
+https://expressjs.com/en/guide/routing.html#express-router
+
+https://expressjs.com/en/starter/static-files.html
+
+https://gist.github.com/alexpchin/102854243cd066f8b88e
+
+https://www.youtube.com/watch?v=MpuF4uPopWI
+
+https://www.youtube.com/watch?v=SccSCuHhOw0
+
+https://nodejs.dev/en/learn/nodejs-file-paths/
+
+https://stackoverflow.com/questions/34329149/nodejs-absolute-paths-in-windows-with-forward-slash
+
+https://www.npmjs.com/package/uuid
+
+https://www.geeksforgeeks.org/node-js-fs-readfilesync-method/
+
+
+
+</details>
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
-const noteDB = require('./db/db.json');
+//const noteDB = require('./db/db.json');
 const fs = require("fs");
 
 const app = express();
@@ -15,12 +15,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'));
 
+//connecting the database
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
+//adds stuff to database
 app.post("/api/notes", (req, res) => {
-
     const newNote = req.body;
     console.log("New Note : " + JSON.stringify(newNote));
     newNote.id = uuidv4();
@@ -30,38 +31,6 @@ app.post("/api/notes", (req, res) => {
     console.log("Added new note to 'db.json' file!");
     res.json(data);
 });
-
-// app.get('/api/notes', (req, res) => {
-//     fs.readFileSync("./db/db.json", (err, data) => {
-//         if (err) {
-//             console.log(err);
-//             res.status(500).json({ error: "something ain't right here buddy"});
-//         } else {
-//             res.json(JSON.stringify(data));
-//             // res.json(JSON.parse(data));
-//         }
-//     })
-//    // res.sendFile(path.join(__dirname, './public/notes.html'));
-// });
-
-//todo fs.writeFile POST request here & parse it somehow
-    //1. pull current note data 
-    //- readFileSync- noteDB     //readFileSync async func/async await- loads up before anything else is done
-    //2. add new note data let newNote is another key in db.json
-    //- use noteDB to add newNotes and parse it with a .push and stringify - req.body.(title/text/id from db.json) how do? .push somewhere 
-    //- need to create id
-    //3. fs.writeFile db.json -- res.json w uuid()
-    //- JSON.parse(fs.readFileSync(./db/db.json or noteDB)
-    //- api/notes/:id
-//     let newNotes = JSON.stringify(fs.writeFileSync("./db/db.json", (err, noteDB) => {
-//         if (err) {
-//             console.log(err);
-//             res.status(500).json({err: "something's broken yo"});
-//         } else {
-//             res.json(JSON.parse(noteDB.push(newNotes)));
-//         }
-//         }));
-/************************************************************************************************************************************/
 
 //connecting the index.html
 app.get('/', (req, res) => {
@@ -80,49 +49,3 @@ app.get('*', (req, res) => {
 
 //connecting the localhost port
 app.listen(PORT, console.log(`Port working properly at ${PORT}`));
-
-
-/********************************************************************************************/
-//todo this is literally all just parsing notes and garbage that isn't working
-// app.post("/api/notes", (req, res) => {
-
-//     const newNote = req.body;
-//     console.log("New Note : " + JSON.stringify(newNote));
-//     newNote.id = uuidv4();
-//     let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-//     data.push(newNote);
-//     fs.writeFileSync('./db/db.json', JSON.stringify(data));
-//     console.log("Added new note to 'db.json' file!");
-//     res.json(data);
-// });
-// app.post('/api/notes', (req, res) => {
-    //readFileSync here
-//     const notes = JSON.stringify(fs.readFileSync("./db/db.json", (err, data) => {
-//         if (err) {
-//             console.log(err);
-//             res.status(500).json({err: "something ain't right here buddy"});
-//         } else {
-//             res.json(JSON.parse(data.JSON.stringify(notes)));
-//         }
-//     }));
-
-// });
-
-        // if (err) {
-        //     console.log(err);
-        //     res.status(500).json({error: "somethin's funky here yo"});
-        // } else {
-        //   (from index.js file)
-        //     const handleNoteSave = () => {
-        //         const newNote = {
-        //           title: noteTitle.value,
-        //           text: noteText.value,
-        //         };
-        //         saveNote(newNote).then(() => {
-        //           getAndRenderNotes();
-        //           renderActiveNote();
-        //         });
-        //       };
-        //       handleNoteSave()
-
-            // res.json(data + newNote);
